a0b4d14e123a4ff25702cf63aa7429c40b42dd8d
StefanoParente <stefano.parente@sou.inteli.edu.br>
2023-02-23 13:58:38 -0300

    tarefa commit

--- exercicio1.py
+++ exercicio1.py
@@ -5,7 +5,13 @@
 
 
 # Crie a função que será avaliada no exercício aqui
+import string
 
+def conta_palavras_unicas(text):
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    words = text.split()
+    words = [word for word in words if not word.isdigit()]
+    return len(words)
 
 
 
--- exercicio2.py
+++ exercicio2.py
@@ -6,6 +6,14 @@
 
 # Crie a função que será avaliada no exercício aqui
 
+def is_anagram(palavra1, palavra2):
+    palavra1 = sorted(palavra1.lower())
+    palavra2 = sorted(palavra2.lower())
+
+    if palavra1==palavra2:
+        return True
+    else:
+        return False
 
 
 
